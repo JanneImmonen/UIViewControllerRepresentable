48a50adfdb0ba2845a49e95b2ff06907885ce874
Janne <immonenjanne@icloud.com>
2022-03-23 14:45:04 +0200

    Initial Commit

---
+++ UIViewControllerRepresentable/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UIViewControllerRepresentable
+//
+//  Created by Janne Immonen on 23.3.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UIViewControllerRepresentable/UIViewControllerRepresentableApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewControllerRepresentableApp.swift
+//  UIViewControllerRepresentable
+//
+//  Created by Janne Immonen on 23.3.2022.
+//
+
+import SwiftUI
+
+@main
+struct UIViewControllerRepresentableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
